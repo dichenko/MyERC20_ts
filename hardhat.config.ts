31e53a04eb47ded1658ec4eea6216ca23881e235
Dinechko <m@dichenko.ru>
2022-07-01 09:38:30 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,10 +7,31 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-etherscan";
-import { MyERC20 } from "./typechain";
 
-import "./tasks/sampleTask.ts";
-import "./tasks/accounts.ts";
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("transfer", "Transfer tokens")
+  .addParam("addressto", "address to transfer")
+  .addParam("amount", "Amount tokens to transfer")
+  .setAction(
+    async ( addressto, amount, hre ) => {
+      const accounts = await hre.ethers.getSigners();
+      const TOKEN = await hre.ethers.getContractFactory("MyToken");
+      const myToken = TOKEN.attach(
+        "0xC2544D5C72b44162561e4c1b336e06cD27CA93c8"
+      );
+      const user = await hre.ethers.getSigners();
+      const success = await myToken.transfer(addressto, amount);
+      console.log("Transfered", amount, " tokens to ", addressto);
+      return success;
+    }
+  );
 
 
 
